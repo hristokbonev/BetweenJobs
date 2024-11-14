c520fef56da1fcdfb3a00d372a48c897f2335db2
Hristo Bonev <hristo.bonev.a63@learn.telerikacademy.com>
2024-11-14 02:56:52 +0200

    main fixes

--- data/database.py
+++ data/database.py
@@ -1,6 +1,5 @@
 from sqlmodel import SQLModel
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URL
  
