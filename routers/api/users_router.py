145b91aa91b773f3ba84c8ffcc50d303785bb2c5
Hristo Bonev <hristo.bonev.a63@learn.telerikacademy.com>
2024-11-14 03:25:29 +0200

    better session method

--- routers/api/users_router.py
+++ routers/api/users_router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from data.models.user import User
-from data.database import get_db
+from data.database import get_session
 
 router = APIRouter()
 
 @router.get("/users/")
-def get_users(db: Session = Depends(get_db)):
+def get_users(db: Session = Depends(get_session)):
     return db.query(User).all()
