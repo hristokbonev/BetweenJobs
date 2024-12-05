2b7c009e96047e0e64ae9a1ed1cf6d6f4cce5a88
Hristo Bonev <hristo.bonev.a63@learn.telerikacademy.com>
2024-12-05 21:23:40 +0200

    Comment out the main execution block in main.py

--- main.py
+++ main.py
@@ -41,6 +41,6 @@ app.include_router(index_router)
 app.include_router(jobs_router)
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000)
