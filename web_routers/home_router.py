1d661337652f6e469b52a08998df097ba68d0232
Hristo Bonev <hristo.bonev.a63@learn.telerikacademy.com>
2024-12-05 20:54:14 +0200

    Refactor project structure: remove VSCode settings, add custom CSS, update HTML links, and implement new routers for home and job posts

---
+++ web_routers/home_router.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter, Request, Depends
+from starlette.templating import Jinja2Templates
+from data.database import get_session
+from sqlmodel import Session
+from utils import attribute_service as ats
+from utils import auth as au
+
+
+index_router = APIRouter(prefix='')
+templates = Jinja2Templates(directory='templates')
+
+
+@index_router.get('/')
+def index(request: Request):
+    return templates.TemplateResponse(request=request, name='index.html')
+    
+
+@index_router.get('/home')
+def index(request: Request, session: Session = Depends(get_session)):
+    locations = list(ats.get_all_locations(session))
+    print(locations)
+    token = request.cookies.get('token')
+    if token:
+        user = au.get_current_user(token)
+        print(user)
+        return templates.TemplateResponse(
+            request=request, 
+            name='home.html',                        
+            context={
+                'request': request, 
+                'user': user,
+                'locations': locations
+            })
+
+    else:
+        return templates.TemplateResponse(request=request, name='home.html')
+    
+    
---
+++ web_routers/jobposts_router.py
@@ -0,0 +1,138 @@
+from typing import Optional
+from fastapi import APIRouter, Request, Depends, Query, Form
+from starlette.templating import Jinja2Templates
+from data.database import get_session
+from sqlmodel import Session
+from utils import auth as au
+from jobposts import jobpost_service as js
+
+
+jobs_router = APIRouter(prefix='/jobposts')
+templates = Jinja2Templates(directory='templates')
+
+
+@jobs_router.get('/')
+def default_view(
+    request: Request, 
+    session: Session = Depends(get_session),
+    page: int = Query(1, ge=1),
+    limit: int = Query(20, ge=1, le=100)
+):
+    job_adds = js.show_all_posts(session=session)
+
+    all_jobs = len(job_adds)
+    # Apply pagination
+    start = (page -1) * limit
+    end = start + limit
+    paginated_jobs = job_adds[start:end]
+    total_pages = (all_jobs + limit - 1) // limit
+
+    token = request.cookies.get('token')
+    context={
+        'request': request, 
+        'jobadds': paginated_jobs,
+        'alladds': all_jobs,
+        'current_page': page,
+        'total_pages': total_pages,
+        'min': min,
+        'filters': {
+            'search_field': None,
+            'keyword': None
+        }
+    }
+
+    if token:
+        user = au.get_current_user(token)
+        context['user'] = user
+
+    return templates.TemplateResponse(
+        request=request,
+        name='job-listings.html', 
+        context=context
+    )
+
+# Utility function
+def _get_search_data(
+    keyword: str = Form(...),
+    search_field: str = Form(...)
+):
+    return keyword, search_field
+
+
+@jobs_router.post('/')
+def search(
+    request: Request, 
+    session: Session = Depends(get_session),
+    page: int = Query(1, ge=1),
+    limit: int = Query(20, ge=1, le=100),
+    form_data = Depends(_get_search_data)
+):
+    keyword, search_field = form_data
+    print(search_field)
+    print(keyword)
+    # Map search_field to function arguments
+    filter_args = {}
+    if search_field and keyword:
+        if search_field in filter_args:
+            filter_args[search_field] = keyword
+    # Get job posts and sum of jobposts
+    job_adds = js.show_all_posts(
+        session=session,
+        **filter_args
+    )
+
+    all_jobs = len(job_adds)
+    # Apply pagination
+    start = (page -1) * limit
+    end = start + limit
+    paginated_jobs = job_adds[start:end]
+    total_pages = (all_jobs + limit - 1) // limit
+
+    token = request.cookies.get('token')
+    context={
+        'request': request, 
+        'jobadds': paginated_jobs,
+        'alladds': all_jobs,
+        'current_page': page,
+        'total_pages': total_pages,
+        'min': min,
+        'filters': {
+            'search_field': search_field,
+            'keyword': keyword
+        }
+    }
+
+    if token:
+        user = au.get_current_user(token)
+        context['user'] = user
+
+    return templates.TemplateResponse(
+        request=request,
+        name='job-listings.html', 
+        context=context
+    )
+
+@jobs_router.get('/{id}')
+def show_jobpost(
+    id: int,
+    request: Request, 
+    session: Session = Depends(get_session)
+):
+    target_job = js.view_job_post_by_id(id, session)
+
+    token = request.cookies.get('token')
+    context={
+        'request': request, 
+        'jobad': target_job,
+        'min': min
+    }
+
+    if token:
+        user = au.get_current_user(token)
+        context['user'] = user
+
+    return templates.TemplateResponse(
+        request=request,
+        name='job-single.html', 
+        context=context
+    )
