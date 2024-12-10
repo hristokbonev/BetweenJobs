33476a48de702936652c5aa0544cdeec13262817
IVAYLO_GETSOV <ivaylo.getsov.a63@learn.telerikacademy.com>
2024-12-10 23:05:25 +0200

    Quick styling fix

--- main.py
+++ main.py
@@ -54,8 +54,8 @@ app.include_router(company_router)
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000)
 
 
